1656e02ac3f31f668d2c5476d0898d06ee1d3619
Peter Elger <elger.peter@gmail.com>
2015-01-26 23:07:25 +0000

    bumped rev

---
+++ build.sh
@@ -0,0 +1,7 @@
+node nscale.js container build nodezoo elasticsearch
+node nscale.js container build nodezoo beanstalk
+node nscale.js container build nodezoo nznpm
+node nscale.js container build nodezoo nzgithub
+node nscale.js container build nodezoo nzindex
+node nscale.js container build nodezoo nzinfo
+node nscale.js container build nodezoo nzweb
