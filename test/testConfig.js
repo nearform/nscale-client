e13a7213e8f6acf31dc038195115193b16887253
jsdevel <js.developer.undefined@gmail.com>
2015-05-25 21:53:00 -0700

    Adding a more descriptive error when config.json is missing.
    * Cleaning up and adding some tests.

--- test/testConfig.js
+++ test/testConfig.js
@@ -14,39 +14,73 @@
 
 'use strict';
 
-var cfg = require('../lib/config');
+var expect = require('chai').expect;
+var resolve = require('path').resolve;
+var configModulePath = '../lib/config';
 
+function cfg() {
+  return require(configModulePath);
+}
 
+var HOME_DIR;
+function resetHomeDir() {
+  process.env[process.platform === 'win32' ? 'USERPROFILE' : 'HOME'] = HOME_DIR;
+}
+
+function setHomeDir(dir) {
+  HOME_DIR = process.env.USERPROFILE || process.env.HOME;
+  process.env[process.platform === 'win32' ? 'USERPROFILE' : 'HOME'] = HOME_DIR;
+
+  process.env.HOME = resolve(__dirname, dir);
+}
+
+function itShouldThrowADescriptiveError() {
+  it('should throw a descriptive error', function() {
+    expect(function() {
+      cfg().getConfig();
+    }).to.throw(/No nscale-client config file found\./);
+  });
+}
 
 describe('config test', function() {
-
-  beforeEach(function(done) {
-    done();
+  afterEach(function() {
+    delete require.cache[require.resolve(configModulePath)];
+    resetHomeDir();
   });
 
+  describe('when ~/.nearform does not exist', function() {
+    beforeEach(setHomeDir.bind(null, './helpers'));
 
-
-  afterEach(function(done) {
-    done();
+    itShouldThrowADescriptiveError();
   });
 
+  describe('when ~/.nearform does exist', function() {
+    describe('when ~/.nscale/config/config.json does not exist', function() {
+      beforeEach(setHomeDir.bind(null, './helpers/nearform-only-config'));
+      itShouldThrowADescriptiveError();
+    });
 
+    describe('when ~/.nscale/config/config.json does exist', function() {
+      beforeEach(setHomeDir.bind(null,
+          './helpers/nearform-and-nscale-client-config'));
 
-  /*
-  it('should create a blank config', function(done) {
-    var config = cfg.getConfig();
-    console.log(config);
-    done();
-  });
-  */
+      it('should create a blank config', function(done) {
+        cfg().getConfig().should.be.an.object;
+        done();
+      });
 
-
-
-  it('should use a nominated host', function(done) {
-    cfg.use('wibble', 1212);
-    var config = cfg.getConfig();
-    console.log('---> ' + JSON.stringify(config));
-    done();
+      describe('when a port is given to use', function() {
+        it('should use the given port', function(done) {
+          var expectedHost = 'wibble';
+          var expectedPort = 3223;
+          cfg().use(expectedHost, expectedPort);
+          var config = cfg().getConfig();
+          config.host.should.equal(expectedHost);
+          config.port.should.equal(expectedPort);
+          done();
+        });
+      });
+    });
   });
 });
 
